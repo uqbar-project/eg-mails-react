3dadee24e98209701b265cf1e7a3795e5aab22f4
Fernando Dodino <fernando.dodino@gmail.com>
2020-10-17 13:52:10 -0300

    Adding icons for mail row

--- src/components/MailsGrid.js
+++ src/components/MailsGrid.js
@@ -1,6 +1,31 @@
+import { Button } from 'primereact/button'
 import { Column } from 'primereact/column'
 import { DataTable } from 'primereact/datatable'
 import React from 'react'
+
+const marcarComoLeidoTemplate = ({ leido }) => {
+  return (
+    leido ? '' : <Button type="button" icon="pi pi-check" className="p-button-secondary" title="Marcar como leído"></Button>
+  )
+}
+
+const recienteTemplate = (rowData) => {
+  return (
+    rowData.esReciente() ?
+      <span title="Reciente" className="p-badge p-badge-info" style={{ display: 'flex', alignItems: 'center', alignContent: 'center', justifyContent: 'center' }}>
+        <i className="pi pi-calendar"></i>
+      </span> : ''
+  )
+}
+
+const leidoTemplate = ({ leido }) => {
+  return (
+    leido ?
+      <span title="No leído" className="p-badge p-badge-warning" style={{ display: 'flex', alignItems: 'center', alignContent: 'center', justifyContent: 'center' }}>
+        <i className="pi pi-eye"></i>
+      </span> : ''
+  )
+}
 
 // Componente que sabe mostrar los mails en una tabla
 export const MailsGrid = ({ mails }) => {
@@ -10,6 +35,9 @@ export const MailsGrid = ({ mails }) => {
       <Column field="emisor" header="Enviado por" sortable></Column>
       <Column field="asunto" header="Asunto" sortable></Column>
       <Column field="texto" header="Texto" sortable></Column>
+      <Column body={recienteTemplate} ></Column>
+      <Column body={leidoTemplate} ></Column>
+      <Column body={marcarComoLeidoTemplate} ></Column>
     </DataTable >
   )
 }
--- src/components/MailsSummary.js
+++ src/components/MailsSummary.js
@@ -1,4 +1,3 @@
-import { Panel } from 'primereact/panel'
 import React from 'react'
 
 // Componente que sabe mostrar los mails nuevos y los de la última semana
@@ -7,15 +6,19 @@ export const MailsSummary = ({ mails }) => {
   const cantidadSinLeer = mails.filter((mail) => !mail.leido).length
 
   return (
-    <Panel>
-      <span className="p-overlay-badge">
-        <i className="pi pi-calendar" style={{ fontSize: '2em' }}></i>
-        <span className="p-badge">{cantidadRecientes}</span>
-      </span>
-      <span className="p-overlay-badge">
-        <i className="pi pi-eye" style={{ fontSize: '2em' }}></i>
-        <span className="p-badge">{cantidadSinLeer}</span>
-      </span>
-    </Panel >
+    <div style={{ display: 'flex', margin: '1em' }}>
+      <div style={{ margin: '1em' }}>
+        <span className="p-overlay-badge" title="Mails recientes">
+          <i className="pi pi-calendar" style={{ fontSize: '2em' }}></i>
+          <span className="p-badge p-badge-info">{cantidadRecientes}</span>
+        </span>
+      </div>
+      <div style={{ margin: '1em' }}>
+        <span className="p-overlay-badge p-m-2" title="Mails sin leer">
+          <i className="pi pi-eye" style={{ fontSize: '2em' }}></i>
+          <span className="p-badge p-badge-warning">{cantidadSinLeer}</span>
+        </span>
+      </div>
+    </div>
   )
 }
