780e6fcd7f4589e31407f5037ae52498af765f13
Fernando Dodino <fernando.dodino@gmail.com>
2022-08-24 07:05:04 -0300

    2022 version

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+import { act } from 'react-dom/test-utils'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
@@ -10,21 +11,21 @@ import { mailService } from './service/mail'
 
 describe('smoke test de la app', () => {
   test('la app levanta', () => {
-    const { getByTestId } = render(<App/>)
-    expect(getByTestId('app')).toBeInTheDocument()
+    render(<App/>)
+    expect(screen.getByTestId('app')).toBeInTheDocument()
   })
 })
 
 describe('tests del mail summary', () => {
 
   test('recupera la cantidad de recientes', () => {
-    const { getByTestId } = render(<MailsSummary mails={mailService.mails} />)
-    expect(getByTestId('cantidad-recientes')).toHaveTextContent('3')
+    render(<MailsSummary mails={mailService.mails} />)
+    expect(screen.getByTestId('cantidad-recientes')).toHaveTextContent('3')
   })
 
   test('recupera la cantidad de mails sin leer', () => {
-    const { getByTestId } = render(<MailsSummary mails={mailService.mails} />)
-    expect(getByTestId('cantidad-sin-leer')).toHaveTextContent('4')
+    render(<MailsSummary mails={mailService.mails} />)
+    expect(screen.getByTestId('cantidad-sin-leer')).toHaveTextContent('4')
   })
 
 })
@@ -34,23 +35,23 @@ describe('tests del mail grid', () => {
 
   test('muestra un mail reciente con el ícono adecuado', () => {
     const mails = mailService.mails
-    const { getByTestId } = render(<MailsGrid mails={mails} />)
+    render(<MailsGrid mails={mails} />)
     const mailReciente = mails.find((mail) => mail.esReciente())
-    expect(getByTestId('reciente' + mailReciente.id)).toBeInTheDocument()
+    expect(screen.getByTestId('reciente' + mailReciente.id)).toBeInTheDocument()
   })
 
   test('muestra un mail no leído con el ícono adecuado', () => {
     const mails = mailService.mails
-    const { getByTestId } = render(<MailsGrid mails={mails} />)
+    render(<MailsGrid mails={mails} />)
     const mailNoLeido = mails.find((mail) => !mail.leido)
-    expect(getByTestId('noLeido' + mailNoLeido.id)).toBeInTheDocument()
+    expect(screen.getByTestId('noLeido' + mailNoLeido.id)).toBeInTheDocument()
   })
 
   test('muestra un botón para marcar como leído un mail no leído', () => {
     const mails = mailService.mails
-    const { getByTestId } = render(<MailsGrid mails={mails} />)
+    render(<MailsGrid mails={mails} />)
     const mailNoLeido = mails.find((mail) => !mail.leido)
-    expect(getByTestId('btnMarcarLeido' + mailNoLeido.id)).toBeInTheDocument()
+    expect(screen.getByTestId('btnMarcarLeido' + mailNoLeido.id)).toBeInTheDocument()
   })
 
 })
@@ -60,8 +61,8 @@ describe('tests del Mail Reader', () => {
   // https://kentcdodds.com/blog/common-mistakes-with-react-testing-library
 
   test('al buscar pasa los mails filtrados a los componentes hijos', async () => {
-    const { getByTestId } = render(<MailReader />)
-    const textSearch = getByTestId('textSearch')
+    render(<MailReader />)
+    const textSearch = screen.getByTestId('textSearch')
     userEvent.type(textSearch, 'luz')
     const spanMail = await screen.findAllByTestId('fecha')
     expect(spanMail.length).toBe(1)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'primeicons/primeicons.css'
 import 'primereact/resources/primereact.min.css'
@@ -8,11 +8,11 @@ import 'primereact/resources/themes/saga-blue/theme.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
