1cf295fe27436f5fa641cb3456b3e422dadd2374
Fernando Dodino <fernando.dodino@gmail.com>
2022-08-24 07:17:07 -0300

    Fix act warning

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
-import { act } from 'react-dom/test-utils'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 import App from './App'
@@ -10,9 +9,12 @@ import { MailsSummary } from './components/MailsSummary'
 import { mailService } from './service/mail'
 
 describe('smoke test de la app', () => {
-  test('la app levanta', () => {
+  test('la app levanta', async () => {
     render(<App/>)
-    expect(screen.getByTestId('app')).toBeInTheDocument()
+    // Nota: el waitFor no es estrictamente necesario para que el test pase,
+    // solamente es para que no aparezca un mensaje de advertencia de usar act() 
+    // para más info: https://javascript.plainenglish.io/you-probably-dont-need-act-in-your-react-tests-2a0bcd2ad65c
+    await waitFor(() => { expect(screen.getByTestId('app')).toBeInTheDocument() })
   })
 })
 
