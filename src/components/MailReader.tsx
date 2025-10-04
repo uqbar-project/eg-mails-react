96369b0aec0fc771ace712b33a0ada7fd788a180
fdodino <fernando.dodino@gmail.com>
2025-10-04 20:03:03 -0300

    Add options to useEffects call

--- src/components/MailReader.tsx
+++ src/components/MailReader.tsx
@@ -1,48 +1,50 @@
 import './MailReader.css'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import { MailsGrid } from './MailsGrid'
 import { MailsSummary } from './MailsSummary'
-import { useOnInit } from 'src/customHooks/hooks'
+// import { useOnInit } from 'src/customHooks/hooks'
 import { Mail } from 'src/domain/mail'
 import { mailService } from 'src/service/mailService'
 
 export const MailReader = () => {
   const [textoBusqueda, setTextoBusqueda] = useState('')
   const [mails, setMails] = useState<Mail[]>([])
-  
-  useOnInit(() => buscarMails(textoBusqueda))
-  
-  const leerMail = async (mail: Mail) => {
-    mail.leer()
-    // no es necesario hacer esto, pero ojo con avisar al backend
-    await mailService.actualizar(mail)
-    setMails([...mails])
-    
-    // setMails(mails.map((mail) =>
-    //   mail.id === idLeido ? mail.leer() : mail
-    // ))
-    // leer debería ser una función que devuelve un nuevo mail
-  }
-  
-  // Variante más simple
+
+  // Opción 1 para disparar la búsqueda de mail - creamos una función async y la llamamos en el botón de búsqueda
   const buscarMails = async (textoBusquedaNuevo: string) => {
     setTextoBusqueda(textoBusquedaNuevo)
     const nuevosMails = await mailService.getMails(textoBusquedaNuevo)
     setMails(nuevosMails)
   }
   
-  // otra variante más rebuscada
-  // es que el InputText tenga el onchange definido como onChange={(event) => setTextoBusqueda(event.target.value)} 
-  //
+  // Opción 2 - más rebuscado: el InputText tiene definido onChange={(event) => setTextoBusqueda(event.target.value)} 
+  // y el useEffect detecta cambios en el texto de búsqueda y se encarga de disparar la búsqueda 
   // useEffect(() => {
   //   const fetchMails = async () => {
   //     const mails = await mailService.getMails(textoBusqueda)
   //     setMails(mails)
   //   }
-
   //   fetchMails()
   // }, [textoBusqueda])
+  /*
+  Opción 1 para la carga inicial - usamos un ref para disparar la carga inicial 
+  */
+  const hasInitializedRef = useRef(false)
+  if (!hasInitializedRef.current) {
+    hasInitializedRef.current = true
+    buscarMails('')
+  }
+
+  /* Opción 2 para la carga inicial - usamos el hook useOnInit que enmascara un useEffect */
+  // useOnInit(() => buscarMails(textoBusqueda))
+
+  const leerMail = async (mail: Mail) => {
+    mail.leer()
+    // en este ejemplo no es necesario pero si tenemos un backend de verdad hay que avisarle
+    await mailService.actualizar(mail)
+    setMails([...mails])
+  }
 
   return (
     <div className="main">
